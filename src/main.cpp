7637dbe72b6f029bfa5b9a8ffcd3928a04a8447d
Jove Beckett <jovebeckett2@gmail.com>
2024-03-14 22:29:40 -0700

    Fixed for newest Jovial version. Thinking of starting from scratch though

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "Jovial/Components/Components2D.h"
+#include "Jovial/Components/Util/LazyAssets.h"
 #include "Jovial/JovialEngine.h"
+#include "Jovial/Renderer/Sprite2DRenderer.h"
 #include "Jovial/Renderer/TextRenderer.h"
 #include "JovialGui.h"
 
@@ -10,8 +12,9 @@ using namespace jovial;
 #define WINDOW_RES Vector2(0, 0)
 
 namespace Fonts {
-    auto *Normal = new Font(fs::Path::fonts() + "jet_brains.ttf", 20);
-}
+    // Font *Normal = new
+    LazyFont Normal(fs::Path::fonts() + "jet_brains.ttf", 20);
+}// namespace Fonts
 
 class World : public Node {
 public:
@@ -30,13 +33,11 @@ public:
     }
 
     void update() override {
-        rendering::draw_circle({5, {label.get_global_position()}});
-
         int d = 0;
-        if (Input::is_action_just_pressed(Actions::J)) {
+        if (Input::is_char_typed('j')) {
             d = 1;
         }
-        if (Input::is_action_just_pressed(Actions::K)) {
+        if (Input::is_char_typed('k')) {
             d = -1;
         }
         if (d) {
@@ -47,8 +48,8 @@ public:
         }
     }
 
-    Label anchor_label = {anchor_to_string(Anchors::TopMiddle), Fonts::Normal};
-    Label label = {"Hello, Gui", Fonts::Normal};
+    Label anchor_label{anchor_to_string(Anchors::TopMiddle), Fonts::Normal.get()};
+    Label label{"Hello, Gui", Fonts::Normal.get()};
 };
 
 int main() {
@@ -57,6 +58,7 @@ int main() {
     game.push_plugin(new GuiPlugin);
     game.push_plugins(&plugins::default_plugins_2d);
     game.push_plugin(new NodePlugin(new World));
+
     game.run();
     return 0;
 }
